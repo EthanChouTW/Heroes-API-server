c1927364c5c8983d764fb3bba3d8803c669dfbbf
Ethan <ethanchoudev@gmail.com>
2016-11-01 17:09:23 +0800

    use eslintrc cause it more strict

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
+'use strict';
 module.exports = () => {
-  let express = require('express'),
-      path = require('path'),
-      seed = require("./seed"),
-      routes = require('./routes'),
-      middleware = require('./middleware'),
-      app = express();
+    let express = require('express'),
+        path = require('path'),
+        seed = require('./seed'),
+        routes = require('./routes'),
+        middleware = require('./middleware'),
+
+        app = express();
 
 
-  app.set('port', process.env.PORT || 3000);
-  app.get('/heroes', middleware.checkAuthenticate, routes.heroesList);
-  app.get('/heroes/:heroId', middleware.checkAuthenticate, routes.singleHero);
+    app.set('port', process.env.PORT || 3000);
+    app.get('/heroes', middleware.checkAuthenticate, routes.heroesList);
+    app.get('/heroes/:heroId', middleware.checkAuthenticate, routes.singleHero);
 
-  return app;
-}
+    return app;
+};
--- routes/index.js
+++ routes/index.js
@@ -1,156 +1,137 @@
-let request = require('request');
+const request = require('request');
 
-  let functions = {};
+const functions = {};
 
-  functions.heroesList = (req, res) => {
+functions.heroesList = (req, res) => {
+  getHeroeslist()
+    .then((heroesList) => {
+        checkAuthorization(heroesList, req, res);
+        })
+        .catch((error) => {
+            res.json(error);
+        });
+};
 
-    getHeroeslist().then(heroesList => {
+functions.singleHero = (req, res) => {
 
-      checkAuthorization(heroesList, req, res);
+    let heroId = req.params.heroId;
 
-    }).catch((error) => {
-      res.json(error);
-    });
-
-
-  };
-
-  functions.singleHero = (req, res) => {
-
-    let heroId = req.params["heroId"];
-
-    getSingleHero(heroId).then(singleHero => {
-
-      checkAuthorization(singleHero, req, res);
-
-    }).catch((error) => {
-      res.json(error);
-    });
+    getSingleHero(heroId)
+        .then(singleHero => {
+            checkAuthorization(singleHero, req, res);
+        })
+        .catch((error) => {
+            res.json(error);
+        });
 
 
-  };
+};
 
 module.exports = functions;
 
-   function checkAuthorization(result, req, res) {
+function checkAuthorization(result, req, res) {
     // collect promises
-     let promises;
-        if (req.isAuthenticate) {
+    let promises;
+    if (req.isAuthenticate) {
 
-          if (typeof req.params["heroId"] === 'undefined') {//list all heroes
+        if (typeof req.params["heroId"] === 'undefined') { //list all heroes
             // add profile for every hero with secret data
-             promises = result.map((element) => {
+            promises = result.map((element) => {
 
-                    const heroId = element['id'];
+                const heroId = element['id'];
 
-                    return  getHeroesProfile(heroId)
-                                .then((profile) => {
+                return getHeroesProfile(heroId)
+                    .then((profile) => {
 
-                                      element["profile"] = profile;
+                        element["profile"] = profile;
 
-                                      return element;
-                                })
-                                .catch((errorMessage) => {
+                        return element;
+                    })
+                    .catch((errorMessage) => {
 
-                                      return element;
-                                });
+                        return element;
+                    });
 
             });
 
-          } else {// single heroes
+        } else { // single heroes
             promises = [getHeroesProfile(req.params["heroId"]).then((profile) => {
-              result["profile"] = profile
+                result["profile"] = profile
 
-              return result;
+                return result;
             }).catch((error) => {
-              return result;
+                return result;
             })];
 
-          }
+        }
 
-          // call the promise there, whether it single or list
-           Promise.all(promises)
-           .then(results => {
-
-              res.json(result);
-          })
-           .catch(err => {            // if error, send public data for user
-
+        // call the promise there, whether it single or list
+        Promise.all(promises)
+            .then(results => {
+                res.json(result);
+            })
+            .catch(err => { // if error, send public data for user
+                res.json(result);
+            });
+    } else { // is not authenticate
+        if (!req.authErrorMessage) { // without Name and Password
             res.json(result);
-          });
-
-       } else {                       // is not authenticate
-
-          if (!req.authErrorMessage) {// without Name and Password
-            res.json(result);
-          } else {                    // invalid Name and Password
-
-            if (!req.params["heroId"]) {// heroes list, typeof result is array
-              result.map(element => {
-                element['profile'] = req.authErrorMessage;
-                return element;
-              })
-            } else {                  // single hero typeof result is dictionary
-              result['profile'] = req.authErrorMessage;
+        } else { // invalid Name and Password
+            if (!req.params["heroId"]) { // heroes list, typeof result is array
+                result.map(element => {
+                    element['profile'] = req.authErrorMessage;
+                    return element;
+                })
+            } else { // single hero typeof result is dictionary
+                result['profile'] = req.authErrorMessage;
             }
+
             res.json(result);
-          }
-
-       }
-
-
-   }
-
-   function getHeroesProfile(heroId) {
-    return new Promise((resolve, reject) => {
-
-      request.get(
-        `http://hahow-recruit.herokuapp.com/heroes/${heroId}/profile`,{},
-        (error, response, body) => {
-          if (!error && response.statusCode == 200) {
-
-            resolve(JSON.parse(body));
-          } else {
-
-            reject(body);
-          }
         }
-      );
-    });
-  }
+    }
+}
 
-  function getHeroeslist() {
+function getHeroesProfile(heroId) {
     return new Promise((resolve, reject) => {
-
-      request.get(
-        `http://hahow-recruit.herokuapp.com/heroes`,{},
-        (error, response, body) => {
-          if (!error && response.statusCode == 200) {
-
-            resolve(JSON.parse(body));
-          } else {
-
-            reject(body);
-          }
-        }
+        request.get(
+            `http://hahow-recruit.herokuapp.com/heroes/${heroId}/profile`, {},
+            (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    resolve(JSON.parse(body));
+                } else {
+                    reject(body);
+                }
+            }
         );
     });
-  };
+}
 
-  function getSingleHero(heroId) {
+function getHeroeslist() {
     return new Promise((resolve, reject) => {
-
-      request.get(
-        `http://hahow-recruit.herokuapp.com/heroes/${heroId}`,{},
-        (error, response, body) => {
-          if (!error && response.statusCode == 200) {
-
-            resolve(JSON.parse(body));
-          } else {
-
-            reject(body);
-          }
-        }
+        request.get(
+            `http://hahow-recruit.herokuapp.com/heroes`, {},
+            (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    resolve(JSON.parse(body));
+                } else {
+                    reject(body);
+                }
+            }
         );
     });
-  }
+}
+
+function getSingleHero(heroId) {
+    return new Promise((resolve, reject) => {
+        request.get(
+            `http://hahow-recruit.herokuapp.com/heroes/${heroId}`, {},
+            (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    resolve(JSON.parse(body));
+                } else {
+                    reject(body);
+                }
+            }
+        );
+    });
+}
